93519558819140f4bafb4d322db3a29ee54dc982
Ariyo Michael <checkariyomichael@gmail.com>
2025-03-07 11:34:19 +0100

    added permissions and changed workflow name

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,5 @@
 sudo apt install cowsay -y
-cowsay -f dragon "Run for cover, I am a dragon...Roar" >> dragon.txt.
+cowsay -f dragon "Run for cover, I am a dragon...Roar" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
